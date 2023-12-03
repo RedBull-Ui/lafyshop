9382353eaec005ee0253a441ec34c895a88c8352
RedBull-Ui <privatk45@gmail.com>
2023-12-03 15:06:47 +0000

    firestore integrer dans tel tool page

--- app.js
+++ app.js
@@ -452,17 +452,21 @@ app.get('/hp', function (req, res) {
 });
 
 // Exemple pour la catégorie "telTools"
-app.get('/telTools', function (req, res) {
-  const sql = 'SELECT * FROM telTools';
+app.get('/telTools', async (req, res)=> {
 
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error(`Erreur lors de la récupération des données : ${err.message}`);
-      res.status(500).send(`Erreur lors de la récupération des données telTools`);
-    } else {
-      res.render('telTools.ejs', { telTools: results });
-    }
-  });
+  // Récupérer les données depuis la collection 'telTools'
+ const telToolsSnapshot = await db.collection('teltools').get();
+ const telTools = telToolsSnapshot.docs.map((doc) => {
+   const produitData = doc.data();
+   return {
+     ...produitData,
+     id: uuidv4(), // Ajoute un nouvel ID unique à chaque produit
+   };
+ });
+
+ // Rendre la vue en utilisant les données récupérées
+ res.render('telTools.ejs', { telTools });
+
 });
 
 // Exemple pour la catégorie "pcTools"
